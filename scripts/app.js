0b2180d3038b3b847b6fa48951c90455c40e89bc
Mike Ulvila <mikeulvila@me.com>
2016-05-06 13:28:28 -0500

    using framework in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,12 @@
 var g = G$('John', 'Doe');
 g.greet().setLang('es').greet(true);
+
+$('#login').click(function() {
+  // create new Greetr Object
+  var loginGrtr = G$('John', 'Doe');
+  // hide login on screen
+  $('#logindiv').hide();
+  // fire off an HTML greeting, passing #greeting as selector and the chosen language
+  // and log welcome as well
+  loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+})
