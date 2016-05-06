03af9c066dfe44504031b7682dc3552cc14d483d
Mike Ulvila <mikeulvila@me.com>
2016-05-05 21:32:37 -0500

    basic function set attaching to global object

--- scripts/Greetr.js
+++ scripts/Greetr.js
@@ -1,3 +1,25 @@
 (function (global, $) {
 
+  var Greetr = function(firstname, lastname, language) {
+    // return new Object
+    return new Greetr.init(firstname, lastname, language);
+
+    }
+
+    // set prototype on object
+    Greetr.prototype = {};
+    // set init function
+    Greetr.init = function(firstname, lastname, language) {
+
+      var self = this;
+      self.firstName = firstname || "";
+      self.lastName = lastname || "";
+      self.language = language || "en";
+
+    }
+
+  Greetr.init.prototype = Greetr.prototype;
+
+  global.Greetr = global.G$ = Greetr;
+
 })(window, jQuery);
